78841bdc9dc5a798f2c2898ea6cf8b16270bb3ac
DianeEndeavors <team@dianeendeavors.com>
2025-11-10 09:02:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -117,6 +117,7 @@ export default function ErrandServiceApp() {
         setUserEstimatedHours(minTime);
       }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pickupCoords, dropoffCoords, errandCoords, signCurrentCoords, signDestinationCoords, serviceType]);
 
   const calculatePricing = () => {
